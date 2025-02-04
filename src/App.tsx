bca5eff2a0c980a981588f33e6f0ee579cb45b9d
Valentin <valentin@valentins-macbook-pro.home>
2025-02-04 17:27:39 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -42,6 +42,7 @@ function App() {
   }, [searchTerm])
 
   const getLanguage = () => {
+    console.log("dfsdf",window.top.location.href,  window.top.location.href.includes('/de'))
     return languageTmp = window.top.location.href.includes('/de')
         ? LANGUAGE.DE
         : LANGUAGE.FR
