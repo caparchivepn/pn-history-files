a0af81d1e285d05e26f4481c739074b7c87ca442
Valentin <valentin@valentins-macbook-pro.home>
2025-02-06 18:55:44 +0100

    fix design

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,19 @@ const LANGUAGE = {
     search: 'Rechercher',
     nothingFound: 'Aucun document ne correspond à votre recherche',
     showMore: 'Afficher plus',
+    importantInfo: 'Info importante',
+    importantInfoDesc:
+      'veuillez saisir le mot complet pour effectuer une recherche dans les documents.',
     key: 'FR',
   },
   DE: {
     search: 'Suchen',
     nothingFound: 'Kein Dokument entspricht Ihrer Suche',
     showMore: 'Mehr anzeigen',
+    importantInfo: 'Wichtige Info',
+    importantInfoDesc:
+      'bitte geben Sie das vollständige Wort ein, um in den Dokumenten zu suchen.',
+
     key: 'DE',
   },
 }
@@ -42,14 +49,12 @@ function App() {
   }, [searchTerm])
 
   const getLanguage = () => {
-    return  window.location.href.includes('=de')
-        ? LANGUAGE.DE
-        : LANGUAGE.FR
+    return window.location.href.includes('=de') ? LANGUAGE.DE : LANGUAGE.FR
   }
 
   const fetchFiles = async (query: string, pageToken = null, reset = false) => {
     try {
-      const languageTmp = getLanguage();
+      const languageTmp = getLanguage()
       const response = await fetch(
         `/api/fetch-files?query=${encodeURIComponent(query)}&language=${
           languageTmp.key
@@ -85,19 +90,39 @@ function App() {
 
   return (
     <div>
-      <div className="container">
-        <div className="mr-3 input relative flex items-center justify-center">
-          <input
-            onChange={(e) => setSearchTerm(e.target.value)}
-            className="flex-1 max-w-full"
-            type="search"
-            value={searchTerm}
-            placeholder={language.search}
-          />
-          <IconSearch />
+      <div className="container max-w-[100rem]">
+        <div className="w-1/2 flex items-center">
+          <div className="mr-3 max-w-[250px] input relative flex items-center justify-center">
+            <input
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className="flex-1 max-w-full"
+              type="search"
+              value={searchTerm}
+              placeholder={language.search}
+            />
+            <IconSearch />
+          </div>
+          {!initLoad && reload && <IconLoading />}
         </div>
-        {!initLoad && reload && <IconLoading />}
-        <div className="max-w-sm ml-auto p-4 border border-yellow-300 bg-yellow-50 text-yellow-800 rounded-md flex items-center space-x-3"><svg xmlns="http://www.w3.org/2000/svg" className="h-20 w-20 text-yellow-500 mt-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M13 16h-1v-4h-1m1 4h.01M12 9h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg><div className="text-sm"><strong>Info importante</strong> écrivez le mot complêt pour le rechercher dans les documents, sinon celui-ci ne sera pas trouvé.</div></div>
+        <div className="w-1/2 max-w-sm ml-auto p-4 pt-1 pb-1 mt-1 border border-yellow-300 bg-yellow-50 text-yellow-800 rounded-md flex items-center space-x-3">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-10 w-10 text-yellow-500 mt-1"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M13 16h-1v-4h-1m1 4h.01M12 9h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            ></path>
+          </svg>
+          <div className="text-[10px]">
+            <strong>{language.importantInfo}</strong>: {language.importantInfoDesc}
+          </div>
+        </div>
       </div>
 
       <div>
